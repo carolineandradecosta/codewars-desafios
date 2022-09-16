f8ea8d46647bcf0585d83f86a2d7d5f2768529ae
Caroline Andrade <andradecostacaroline@gmail.com>
2022-09-16 00:00:11 -0300

    Adicionar descrição sobre o desafio

--- src/main/kotlin/15-StringRepeat.kt
+++ src/main/kotlin/15-StringRepeat.kt
@@ -1,9 +1,14 @@
 
+//DESCRIPTION:
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+//
+//Examples (input -> output)
+//6, "I"     -> "IIIIII"
+//5, "Hello" -> "HelloHelloHelloHelloHello"
 fun main(){
     println(repeatStr(6, "Mel"))
 }
 
 fun repeatStr(r: Int, str: String): String{
     return str.repeat(r)
-
 }
